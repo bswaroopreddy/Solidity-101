fd37e423e86913f58d5801f1dd78069445f437c1
bswaroopreddy <swaroopreddyb3@gmail.com>
2025-02-24 15:41:21 +0530

    receive fallback

--- FundMe.sol
+++ FundMe.sol
@@ -16,65 +16,35 @@ error noOwner();
 
 // constant, immutability
 import {PriceConverter} from "./priceConverter.sol";
+
 contract FundMe {
+
     using PriceConverter for uint256;
+
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
     
     // constant and immutable variables store directly into the bytecode instead of storage
-    uint256 public constant MINIMUM_USD = 5e18;
+    uint256 public constant MINIMUM_USD = 5 * 1000000000000000000;
     // 347 - constant
     // 2446 - non-constant
-
-
-    address[] public funders;
-    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
     address public immutable i_owner;
     // 439 gas - immutable
     //  2574 gas - non-immutable
 
+    
     constructor() {
         i_owner = msg.sender;
     }
-    
+
     // Allow function to sent value (payable) in native cryptocurrency
     function fund() public payable {
-        // Allow users to send $
-        // Have a minimum $ sent
-
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "didn't send enough ETH");  // 1e18 = 1 ETH = 1000000000000000000 = 1 * 10^18
-
-        // what is a revert?
-        // Undo any actions that have been done, and send the remaining gas back
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
+        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
+        addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
-
-     function withdraw() public onlyOwner{
-        // require(msg.sender == owner, "must be owner");
-        
-        // for loop
-        // [1, 2, 3, 4] elements
-        // 0, 1, 2, 3 indexes
-        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
-            address funder = funders[funderIndex];
-            addressToAmountFunded[funder] = 0;
-        }
-
-        // reset the array
-        funders = new address[](0);
-        // actually withdraw the funds
-
-        // tramsfer
-        payable(msg.sender).transfer(address(this).balance);
-
-        // send
-        bool sendSucceess =  payable(msg.sender).send(address(this).balance);
-        require(sendSucceess, 'Send failed');
-
-        // call
-       (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
-        require(callSuccess, "Call failed");
-     }
 
     modifier onlyOwner() {
         // require(msg.sender == i_owner, "Sender is not owner!");
@@ -83,4 +53,34 @@ contract FundMe {
         }
         _;
     }
+
+     function withdraw() public onlyOwner {
+        // require(msg.sender == owner, "must be owner");
+        
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        // // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+
+        // call
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+
+    
+
+    receive() external payable { 
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+     }
 }
